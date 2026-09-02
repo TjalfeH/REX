232016c83d7e4d2f7c1b25fb261854be9cc605d5
LeonBrodersen <brodersen2006@gmail.com>
2026-09-02 13:54:25 +0200

    ads

--- ex2.py
+++ ex2.py
@@ -5,15 +5,29 @@ arlo = robot.Robot()
 
 print("Running ...")
 
-def circle():
+def rightCircle():
 
-    leftSpeed = 100
-    rightSpeed = 80
+    leftSpeed = 80
+    rightSpeed = 40
     for i in range(4):
-        print(arlo.go_diff(leftSpeed, rightSpeed/2, 1, 1))
-        sleep(0.5)
+        print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+        sleep(2.3)
     
     print(arlo.stop())
 
-circle()
+def leftCircle():
+
+    leftSpeed = 40
+    rightSpeed = 80
+    for i in range(4):
+        print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+        sleep(2.3)
+    
+    print(arlo.stop())
+
+for i in range(4):
+    leftCircle()
+    sleep(0.5)
+    rightCircle()
+    sleep(0.5)
 arlo.stop()
