180cd8515a582c86c07300fec0809bf30d3a68d1
LeonBrodersen <brodersen2006@gmail.com>
2026-09-02 13:20:06 +0200

    ex

--- ex2.py
+++ ex2.py
@@ -4,3 +4,13 @@ from time import sleep
 arlo = robot.Robot()
 
 print("Running ...")
+
+def circle():
+
+    leftSpeed = 80
+    rightSpeed = 80
+    for i in range(4):
+        print(arlo.go_diff(leftSpeed, rightSpeed/2, 1, 1))
+        sleep(0.5)
+    
+    print(arlo.stop())
