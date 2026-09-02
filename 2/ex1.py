24a2351d7b6bee67dfc3a459a21b86c242fce252
LeonBrodersen <brodersen2006@gmail.com>
2026-09-02 14:16:39 +0200

    erf

--- 2/ex1.py
+++ 2/ex1.py
@@ -4,3 +4,16 @@ from time import sleep
 arlo = robot.Robot()
 
 print("Running ...")
+def driveForward():
+    leftSpeed = 40
+    rightSpeed = 40
+    print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
+
+    sleep(0.5)
+    print(arlo.stop())
+
+for i in range(4):
+    driveForward()
+    print(arlo.read_front_ping_sensor())
+    sleep(0.5)
+arlo.stop()
