66528e7b8a04c7372df6638fbc4096ec5fe73638
ethangunn <ethangunncv@gmail.com>
2026-09-16 14:17:20 +0200

    Camera Test Added

---
+++ cameraTest.py
@@ -0,0 +1,31 @@
+import time
+
+import cv2
+import picamera2
+import robot
+
+marker_length = 0.055
+camera_matrix = [[1800,  0, 820],
+                 [ 0, 1800, 616],
+                 [ 0,  0,  1]]
+dist_coeffs = [0,0,0,0,0]
+
+arlo = robot.Robot()
+cam = picamera2.Picamera2()
+cam.configure(cam.create_video_configuration({"size": (1640, 1232), "format": "RGB888"}, queue=False))
+cam.start()
+time.sleep(1)
+
+aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+
+corners, ids, _ = cv2.aruco.detectMarkers(cam.capture_array(), aruco_dict)
+rvecs, tvecs, _objPoints = cv2.aruco.estimatePoseSingleMarkers(
+corners, marker_length, camera_matrix, dist_coeffs
+)
+
+for tvec in tvecs:
+    x, y, z = tvec[0]
+    distance = numpy.sqrt(x**2 + y**2 + z**2)
+    print(f"Distance to marker: {distance:.3f} meters")
+
+print(distance)
