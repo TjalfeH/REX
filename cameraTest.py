d1f660f0fe4950c152c9239fe5ac3566830f3b51
ethangunn <ethangunncv@gmail.com>
2026-09-16 14:24:03 +0200

    Camera test updated

--- cameraTest.py
+++ cameraTest.py
@@ -19,7 +19,7 @@ time.sleep(1)
 aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
 
 corners, ids, _ = cv2.aruco.detectMarkers(cam.capture_array(), aruco_dict)
-rvecs, tvecs, _objPoints = cv2.aruco.estimatePoseSingleMarkers(
+rvecs, tvecs = cv2.aruco.estimatePoseSingleMarkers(
 corners, marker_length, camera_matrix, dist_coeffs
 )
 
