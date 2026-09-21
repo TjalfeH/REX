e48c0f6b9dda0a969d85a489b19643c219e0aab6
LeonBrodersen <brodersen2006@gmail.com>
2026-09-21 14:43:17 +0200

    qw

--- localGrid.py
+++ localGrid.py
@@ -47,6 +47,8 @@ for i in range(len(ids)):
     py = int(750 - z * SCALE)
     cv2.circle(world_map, (px, py), int(Box_Radius * SCALE), (255, 0, 0), -1)
     cv2.putText(world_map, "ID " + str(marker_id), (px + 15, py + 5), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
+    cv2.circle(world_map, (px, py), int(Robot_Radius * SCALE), (0, 255, 0), 2)
+    cv2.circle(world_map, (px, py), int(Box_Radius * SCALE), (0, 0, 255), 2)
 
 def in_collision(x, y):
     for (lx, lz) in landmark:
