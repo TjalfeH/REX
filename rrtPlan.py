237f4328e0aba68217536030d72d98d26f664ac4
LeonBrodersen <brodersen2006@gmail.com>
2026-09-23 14:14:31 +0200

    tf

--- rrtPlan.py
+++ rrtPlan.py
@@ -39,8 +39,8 @@ for meter in range(-2, 3):
     cv2.putText(world_map, str(meter) + " m", (x_pixel + 5, 790), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0))
 
 landmark = []
-Box_Radius = 0.15
-Robot_Radius = 0.25
+Box_Radius = 0.2
+Robot_Radius = 0.3
 
 for i in range(len(ids)):
     marker_id = int(ids[i][0])  
